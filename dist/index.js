c02f3c5c92e62332b826e95886cdddbe26f92f2d
jeff <jeff@giftbit.com>
2017-02-07 14:24:03 -0800

    ... and committing the build.

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 const jwtauth = require("./jwtauth");
 exports.jwtauth = jwtauth;
+const secureConfig = require("./secureConfig");
+exports.secureConfig = secureConfig;
 //# sourceMappingURL=index.js.map
