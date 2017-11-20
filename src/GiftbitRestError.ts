3bf39057d1feac0a4d0b87c6e9e3aee90565dc80
jeff <jeff@giftbit.com>
2017-11-20 10:04:35 -0800

    comment

--- src/GiftbitRestError.ts
+++ src/GiftbitRestError.ts
@@ -1,5 +1,8 @@
 import * as cassava from "cassava";
 
+/**
+ * A strongly-typed wrapped around cassava.RestError that standardizes our `messageCode` convention.
+ */
 export class GiftbitRestError extends cassava.RestError {
 
     constructor(statusCode?: number, message?: string, messageCode?: string) {
