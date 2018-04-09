28f75ee60c55403b9ae2efad35870a5d7c5e5402
jeff <jeff@giftbit.com>
2018-04-09 15:59:27 -0700

    Release 5.5.2.  Export GiftbitRestError from index.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as jwtauth from "./jwtauth";
 import * as secureConfig from "./secureConfig";
-import {HealthCheckRoute} from "./HealthCheckRoute";
+export {GiftbitRestError} from "./GiftbitRestError";
+export {HealthCheckRoute} from "./HealthCheckRoute";
 
-export {jwtauth, secureConfig, HealthCheckRoute};
+export {jwtauth, secureConfig};
