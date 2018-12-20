5a1b5a05656c19aefb9b5e97ae88b7677d0fc0d9
jeff <jeff@giftbit.com>
2018-12-20 14:28:47 -0800

    Release 10.0.2.  Trying to fix issues with not getting expected error notifications in Sentry.

--- dist/sentry/index.js
+++ dist/sentry/index.js
@@ -85,10 +85,10 @@ function sendErrorNotificationImpl(err) {
     return __awaiter(this, void 0, void 0, function* () {
         logger(err);
         yield initializedPromise;
-        return new Promise(((resolve, reject) => {
+        return yield new Promise(((resolve, reject) => {
             Raven.captureException(err, ravenContext, (ravenError) => {
                 if (ravenError) {
-                    logger("error sending to Sentry", ravenError);
+                    logger("Unable to send to Sentry:", ravenError);
                     reject(ravenError);
                 }
                 else {
