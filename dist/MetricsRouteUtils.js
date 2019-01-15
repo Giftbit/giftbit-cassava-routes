38fbf92ba654826173995fa3bb2188bc7b18291e
Tana Jukes <tana.j@lightrail.com>
2019-01-15 13:03:20 -0800

    Comment and refactor confusing function

--- dist/MetricsRouteUtils.js
+++ dist/MetricsRouteUtils.js
@@ -8,15 +8,29 @@ function getPathForMetricsLogging(evt, handlingRoutes) {
         path = pathRegex.toString();
         if (handler.settings.regexGroupToPathParamMap) {
             let pathParams = handler.settings.regexGroupToPathParamMap;
-            let indexArray = [];
-            function replacer(...args) {
-                indexArray.push(args[1]);
-                return `<${pathParams[indexArray.length]}>`;
-            }
-            path = path.replace(/\(.+?\)(?=\\\/|\$)/g, replacer);
+            let pathParamIndex = 0;
+            /**
+             * Replace each path parameter in the stringified path regex with the corresponding parm param identifier
+             *
+             * For example: if evt.path is "/v2/programs/program-id-12345",`
+             *      the matching regexp is /^\/v2\/programs\/([0-9a-zA-Z-._~!$&'()*+,;=:@%]+)$/
+             *      and the pathParams are ["", "programId"] (see https://github.com/Giftbit/cassava/blob/v2.4.0/src/routes/BuildableRoute.ts#L37)
+             * This will replace "([0-9a-zA-Z-._~!$&'()*+,;=:@%]+)" with "<programId>" so 'path' will be "/^\/v2\/programs\/<programId>$/"
+             */
+            path = path.replace(/\(.+?\)(?=\\\/|\$)/g, // matches capture groups followed by an escaped slash or end of string
+            () => {
+                pathParamIndex += 1;
+                return `<${pathParams[pathParamIndex]}>`;
+            });
+        }
+        else {
+            // no path parameters to replace
         }
         path = path.replace(/\\\//g, "/"); // replace escaped slashes
         path = path.replace(/\/\^|\$\/[gimuy]/g, ""); // clear start/end markers & flags
+    }
+    else {
+        // this request didn't get handled by a route with a path matcher: no path replacements can be made
     }
     return path;
 }
